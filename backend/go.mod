7747fb0e8cf0e0a4552f9a68e725074421bb8e20
lemonade-2061 <lemonade.2061.7.29@gmail.com>
2026-05-18 02:33:38 +0900

    環境構築:astro frontend + go backend

---
+++ backend/go.mod
@@ -0,0 +1,3 @@
+module github.com/lemonade-2061/lemonade.dev
+
+go 1.26.2
---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            nodejs
+            pnpm
+            go
+            postgresql
+            sqlc
+            air
+          ];
+        };
+      }
+    );
+}
